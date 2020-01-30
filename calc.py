4ff2432fd7b42d0213adc34f8fb8e0e4ceaf8324
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-30 13:34:56 -0800

    building basic calculator

---
+++ calc.py
@@ -0,0 +1,24 @@
+
+class Mycalculator(object):
+    pass
+
+    def __init__(self):
+        pass
+
+    def add(self, n1, n2):
+        return n1 + n2
+
+    def subs(self, n1, n2):
+        return n1-n2
+
+    def mult(self, n1, n2):
+        return n1*n2
+
+    def div(self, n1, n2):
+        return n1/n2
+
+
+if __name__ == '__main__':
+    calc = Mycalculator()
+    print(calc.add(1,2))
+
