a82951c310e81c10b8b07307c7c1204897b490d4
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-24 10:26:48 -0800

    printing by max value, very close

--- chain_business_problem/max_chain_business.py
+++ chain_business_problem/max_chain_business.py
@@ -12,7 +12,6 @@ class Business():
         return (f"name: {self.name}, location: {self.location}, id: {self._id}")
 
 
-
 businesses = [
     Business("Sturbucks", "SF", 100),
     Business("Sturbucks", "SF", 100),
@@ -29,8 +28,9 @@ businesses = [
 
 
 def num_of_chains(list_of_businesses, city):
-    
+
     all_chains = {}
+    # listed = []
     for business in list_of_businesses:
         if city == business.location and business.name not in all_chains:
             all_chains[business.name] = 1
@@ -38,27 +38,31 @@ def num_of_chains(list_of_businesses, city):
             all_chains[business.name] += 1
     return all_chains
 
+# [
+    # {'Sturbucks': 2},
+    # {'Whole Foods': 3},
+    # {'Amazon': 2},
+    # {'McDonalds': 2}
+# ]
 
-def sort_chain(list_of_businesses, city):
 
+def sort_output(list_of_businesses, city):
     dict_of_chains = num_of_chains(list_of_businesses, city)
+    sorted_dict = sorted(dict_of_chains.items(), key=lambda x: x[1])
 
-    for name, num in dict_of_chains.items():
-        print(name, num)
-
-    
-
-
+    last_indx = len(sorted_dict) - 1
+    while last_indx >= 0:
+        print(sorted_dict[last_indx])
+        last_indx -= 1
 
 
     # create dictionary
     # iterate throught the list
     # if the input city matches the busines location
-    # append dictionary key = business name, value = how many 
-    
+    # append dictionary key = business name, value = how many
+
     # iterate over the dict
     # print key value pair
 
 
-print(sort_chain(businesses, "SF"))
-
+print(sort_output(businesses, "SF"))
